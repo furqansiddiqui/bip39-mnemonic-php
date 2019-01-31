1db065faec0a33114f73a92236579c1d41a4bcc8
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-01-31 21:05:53 +0500

    LICENSE

--- src/Wordlist.php
+++ src/Wordlist.php
@@ -11,13 +11,3 @@
  */
 
 declare(strict_types=1);
-
-namespace furqansiddiqui\BIP39\Exception;
-
-/**
- * Class BIP39_Exception
- * @package furqansiddiqui\BIP39\Exception
- */
-class BIP39_Exception extends \Exception
-{
-}
